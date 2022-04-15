161cdeda080304cc7465d714e8b6576d12c7c97b
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-04-15 13:39:44 -0400

    changed routes back and removed s in URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import PostList from './components/PostList'
 import AddPost from './components/AddPost'
-import ViewPost from './components/Show'
+import ViewPost from './components/ViewPost'
 import EditPost from './components/EditPost'
 
 
@@ -11,14 +11,14 @@ function App() {
   return (
     <Router>
       <nav id='nav'>
-        <Link to='https://tonyford2002.github.io/Sailing-Blog/'> Home </Link>
-        <Link to='https://tonyford2002.github.io/Sailing-Blog/addPost'> Add A Post </Link>
+        <Link to='/'> Home </Link>
+        <Link to='/addPost'> Add A Post </Link>
       </nav>
       <Routes>
-        <Route path= 'Sailing-Blog/' element={<PostList/>}></Route>
-        <Route path= 'Sailing-Blog/addPost' element={<AddPost/>}></Route>
-        <Route path= 'Sailing-Blog/:id' element={<ViewPost/>}></Route>
-        <Route path= 'Sailing-Blog/:id/editRecord' element={<EditPost/>}></Route>
+        <Route path= '/' element={<PostList/>}></Route>
+        <Route path= '/addPost' element={<AddPost/>}></Route>
+        <Route path= '/:id' element={<ViewPost/>}></Route>
+        <Route path= '/:id/editRecord' element={<EditPost/>}></Route>
       </Routes>
     </Router>
   )
